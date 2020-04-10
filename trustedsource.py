bcff422dea583e6952f778c170675652b713048a
Jasmeet Singh Saroya <jsinix@users.noreply.github.com>
2020-04-09 22:48:32 -0400

    Update trustedsource.py

--- trustedsource.py
+++ trustedsource.py
@@ -1,46 +1,82 @@
-import requests
+import requests, sys, argparse, os
+from tqdm import tqdm
 from bs4 import BeautifulSoup
+from multiprocessing.dummy import Pool as ThreadPool
 
-def setup():
-    headers = {
-    'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_5)',
-    'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-    'Accept-Language' : 'en-US,en;q=0.9,de;q=0.8'
-    }
+# Reference: https://github.com/mohlcyber/TrustedSource-Service-Lookup
+headers = {'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_5)', 
+'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+'Accept-Language' : 'en-US,en;q=0.9,de;q=0.8'}
+base_url = 'http://www.trustedsource.org/sources/index.pl'
+r = requests.get(base_url, headers=headers)
+bs = BeautifulSoup(r.content, "html.parser")
+form = bs.find("form", { "class" : "contactForm" })
+token1 = form.find("input", {'name': 'e'}).get('value')
+token2 = form.find("input", {'name': 'c'}).get('value')
+headers['Referer'] = base_url
 
-    base_url = 'http://www.trustedsource.org/sources/index.pl'
-    r = requests.get(base_url, headers=headers)
+def writedata(entry, filename):
+    with open(filename, "a") as myfile:
+        myfile.write(entry+'\n')
 
-    bs = BeautifulSoup(r.content, "html.parser")
-    form = bs.find("form", { "class" : "contactForm" })
-    token1 = form.find("input", {'name': 'e'}).get('value')
-    token2 = form.find("input", {'name': 'c'}).get('value')
-
-    headers['Referer'] = base_url
-    return headers, token1, token2
-
-def lookup(headers, token1, token2, url):
+def lookup(url):
+    pbar.update(1)
     payload = {'e':(None, token1),
                'c':(None, token2),
                'action':(None,'checksingle'),
                'product':(None,'01-ts'),
                'url':(None, url)}
 
-    r = requests.post('https://www.trustedsource.org/en/feedback/url', headers=headers, files=payload)
+    try:
+        r = requests.post('https://www.trustedsource.org/en/feedback/url', headers=headers, files=payload)
+        bs = BeautifulSoup(r.content, "html.parser")
+        form = bs.find("form", { "class" : "contactForm" })
+        table = bs.find("table", { "class" : "result-table" })
+        td = table.find_all('td')
+        categorized = (td[len(td)-3].text).lstrip().rstrip()
+        category = (td[len(td)-2].text[2:]).lstrip().rstrip()
+        risk = (td[len(td)-1].text).lstrip().rstrip()
+        data = ('{},{},{},{}'.format(url,categorized, category, risk))
+        writedata(data, trustedsource-results.csv)
+        #print data
+    except Exception as err:
+        error = '{},{}' .format(url,err)
+        writedata(error, 'trustedsource-error.csv')
 
-    bs = BeautifulSoup(r.content, "html.parser")
-    form = bs.find("form", { "class" : "contactForm" })
+def process_arguments(args):
+    parser = argparse.ArgumentParser(description="CLI Tool To Query Website Reputation")
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('-f',
+                        '--file',
+                        help="File with hostnames, each separated by a new line"
+                        )
+    group.add_argument('-H',
+                        '--host',
+                        help="Single host to query"
+                        )
+    options = parser.parse_args(args)
+    return vars(options)
 
-    table = bs.find("table", { "class" : "result-table" })
-    td = table.find_all('td')
-    categorized = td[len(td)-3].text
-    category = td[len(td)-2].text[2:]
-    risk = td[len(td)-1].text
+if len(sys.argv) < 2:
+    process_arguments(['-h'])
+userOptions = process_arguments(sys.argv[1:])
 
-    return categorized, category, risk
+try:
+    os.system('clear')
+except Exception as err:
+    os.system('cls')
+print "\n\n\n\t\tMcAfee TrustedSource Category Check\n\n"
 
-if __name__ == "__main__":
-    url = raw_input('Enter a valid domain / url to check: ')
-    headers, token1, token2 = setup()
-    categorized, category, risk = lookup(headers, token1, token2, url)
-    print('\033[1m' + 'Staus: {0}     |     Category: {1}     |     Risk: {2}'.format(categorized, category, risk) + '\033[0m')
+if userOptions['host'] != None:
+    url = userOptions['host'].lstrip().rstrip() 
+    categorized, category, risk = lookup(url)
+    print('{},{},{},{}'.format(url,categorized, category, risk))
+
+else:
+    file = userOptions['file']
+    hosts =  [x.replace('\n', '').replace('[.]', '.').lstrip().rstrip() for x in open(file)]
+    pbar = tqdm(total=len(hosts))
+    pool = ThreadPool(25)
+    results = pool.map(lookup, hosts) 
+    pool.close()
+    pool.join() 
